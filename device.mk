7ea0a22109b25eb12381f7df4b1c3470e2c203e7
Sarthak Roy <sarthakroy2002@gmail.com>
2025-01-04 19:59:37 +0530

    peridot: Switch to libperfmgr from hardware/google
    * Also drop no-op imports
    
    Change-Id: I35d58574826f45de95403e63f4979066
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -365,7 +365,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
@@ -395,7 +395,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     vendor/qcom/opensource/usb/etc
 
 # Sensors
