32f697c412346676f58f3d142249d885542b5102
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-04-30 02:04:23 +0200

    peridot: Enable CameraX extensions support
    - Set `ro.camerax.extensions.enabled=true` in both system.prop and odm.prop
    - Add `camerax-vendor-extensions.jar` to system_ext
    - Grant required permission via `camerax-vendor-extensions.xml`
    
    Enables support for enhanced camera features via CameraX Extensions, improving compatibility with modern camera apps.
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -132,6 +132,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     DSPVolumeSynchronizer
 
+# CameraExtensions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer3-V1-ndk.vendor \
