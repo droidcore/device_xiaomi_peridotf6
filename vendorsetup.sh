3caf77e6fa8f32c936ffb66a1ee25fd7c41223c6
JuanArton <juanartoN@protonmail.com>
2025-11-11 14:07:07 +0000

    vendorsetup: clone gms and key

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,8 +32,11 @@ git clone -b lineage-23.0 https://github.com/lightbulb-box/android_packages_apps
 # ViperFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
-#lineage-key
+# Lineage-Key
 git clone git@github.com:lightbulb-box/peridot-key.git vendor/lineage-priv/keys
+
+# Gapps 
+git clone https://codeberg.org/Zabuka/vendor_gms vendor/gms
 
 # Fix deprecated camera override flag
 BOARD_CONFIG=device/xiaomi/peridot-miuicamera/BoardConfig.mk
