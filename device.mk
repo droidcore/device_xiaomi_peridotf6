acd63cf017d888271e27b63cdf10a99163d6072c
droidcore <agusmahardika99@gmail.com>
2025-11-25 09:04:23 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -163,9 +163,6 @@ PRODUCT_PACKAGES += \
 SYSTEM_OPTIMIZE_JAVA = true
 SYSTEMUI_OPTIMIZE_JAVA = true
 
-# FastCharge
-$(call inherit-product, packages/apps/FastCharge/fastcharge.mk)
-
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
@@ -298,6 +295,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
+
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/fastcharge_enable)
+$(call soong_config_set,lineage_health,fast_charge_value_none,0)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,1)
+$(call soong_config_set,lineage_health,fast_charge_value_super_fast_charge,2)
 
 # Media
 PRODUCT_COPY_FILES += \
