86a9533417b844e51c33b80660eac2fc6425a404
droidcore <agusmahardika99@gmail.com>
2025-11-28 11:11:07 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -494,6 +494,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Userdata
+PRODUCT_FS_COMPRESSION := 1
+
 # VNDK
 PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V3-ndk.vendor \
