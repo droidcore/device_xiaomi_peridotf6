76426ab6ab0234dcdc778fb5c9ab30985c92f40f
droidcore <agusmahardika99@gmail.com>
2025-12-01 05:36:41 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -300,11 +300,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/fastcharge_enable)
-$(call soong_config_set,lineage_health,fast_charge_value_none,0)
-$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,1)
-$(call soong_config_set,lineage_health,fast_charge_value_super_fast_charge,2)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,fast_charge_node,/sys/class/qcom-battery/fastcharge_enable)
+$(call soong_config_set_bool,lineage_health,fast_charge_value_none,0)
+$(call soong_config_set_bool,lineage_health,fast_charge_value_fast_charge,1)
+$(call soong_config_set_bool,lineage_health,fast_charge_value_super_fast_charge,2)
 
 # Media
 PRODUCT_COPY_FILES += \
